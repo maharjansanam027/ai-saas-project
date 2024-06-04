24638d5ade0f76fe7a19fc53564a96c232a60927
maharjansanam027 <108982463+maharjansanam027@users.noreply.github.com>
2024-06-04 14:43:05 +0530

    initial commit

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("8254f25a-eca0-421e-b4de-2bf655c03887");
+    Crisp.configure("c1d0b782-1eec-4db9-9156-6811e6cc7561");
   }, []);
 
   return null;
